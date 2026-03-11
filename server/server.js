a32cea12ed85de267fb7edba4d1852134657f2d6
Aakhyya <aakhyya@gmail.com>
2026-03-11 12:11:47 +0530

    chore: added MongoDB connection and folder setup

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const app = require("./app");
 const PORT= process.env.PORT || 8001;
+const connectDB=require("./config/db");
 
+connectDB();
 app.listen(PORT, () => {
   console.log(`Server Started at PORT:${PORT}`);
   console.log(`Environment: ${process.env.NODE_ENV || "development"}`);
